72abb291bbbbe4a923e9b6db8fe7d766ad5db4c6
Paul Wilkinson <paul.wilkinson3@ocado.com>
2024-09-10 07:25:22 +0200

    Individual oscillator panels for each oscillator

--- src/ControlPanel.tsx
+++ src/ControlPanel.tsx
@@ -3,16 +3,55 @@ import { useStore } from './store';
 import { Slider } from './components/ui/slider';
 import { HuePicker } from 'react-color';
 
-export const ControlPanel = () => {
+interface OscillatorSettingsProps {
+    oscId: number;
+}
+
+const OscillatorSettings = ({ oscId }: OscillatorSettingsProps) => {
     const {
         controlSettings,
-        setBalance, 
         setColor,
         setFrequency,
         setSpeed,
         setSharpen,
         setCenterX,
         setCenterY,
+    } = useStore();
+
+    return (
+        <>
+        <div className='flex justify-center w-full my-2 mt-4'>OSC 1</div>
+        <div className="flex items-center row">
+            <HuePicker color={controlSettings.oscillators[oscId].color} onChange={(color) => { setColor(0, color.hex); }} />
+        </div>
+        <div className="flex items-center row">
+            <div className="flex w-16 mx-2">Freq</div>
+            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[oscId].frequency]} max={10.0} min={0.0} step={0.01} onValueChange={(value) => { setFrequency(oscId, value[0]); }} />
+        </div>
+        <div className="flex items-center row">
+            <div className="flex w-16 mx-2">Speed</div>
+            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[oscId].speed]} max={30.0} min={0.0} step={0.01} onValueChange={(value) => { setSpeed(oscId, value[0]); }} />
+        </div>
+        <div className="flex items-center row">
+            <div className="flex w-16 mx-2">Sharp</div>
+            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[oscId].sharpen]} max={4.0} min={1.0} step={0.01} onValueChange={(value) => { setSharpen(oscId, value[0]); }} />
+        </div>
+        <div className="flex items-center row">
+            <div className="flex w-16 mx-2">X</div>
+            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[oscId].center[oscId]]} max={100.0} min={-100.0} step={0.01} onValueChange={(value) => { setCenterX(oscId, value[0]); }} />
+        </div>
+        <div className="flex items-center row">
+            <div className="flex w-16 mx-2">Y</div>
+            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[oscId].center[1]]} max={100.0} min={-100.0} step={0.01} onValueChange={(value) => { setCenterY(oscId, value[0]); }} />
+        </div>
+        </>
+    );
+}
+
+export const ControlPanel = () => {
+    const {
+        controlSettings,
+        setBalance, 
     } = useStore();
 
     useEffect(() => {
@@ -25,54 +64,8 @@ export const ControlPanel = () => {
         <div className="flex items-center row">
             <Slider defaultValue={[controlSettings.balance]} max={1.0} min={0.0} step={0.01} onValueChange={(value) => { setBalance(value[0]); }} />
         </div>
-        <div className='flex justify-center w-full my-2 mt-4'>OSC 1</div>
-        <div className="flex items-center row">
-            <HuePicker color={controlSettings.oscillators[0].color} onChange={(color) => { setColor(0, color.hex); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Freq</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[0].frequency]} max={10.0} min={0.0} step={0.01} onValueChange={(value) => { setFrequency(0, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Speed</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[0].speed]} max={30.0} min={0.0} step={0.01} onValueChange={(value) => { setSpeed(0, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Sharp</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[0].sharpen]} max={4.0} min={1.0} step={0.01} onValueChange={(value) => { setSharpen(0, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">X</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[0].center[0]]} max={100.0} min={-100.0} step={0.01} onValueChange={(value) => { setCenterX(0, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Y</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[0].center[1]]} max={100.0} min={-100.0} step={0.01} onValueChange={(value) => { setCenterY(0, value[0]); }} />
-        </div>
-        <div className='flex justify-center w-full my-2 mt-4'>OSC 2</div>
-        <div className="flex items-center row">
-            <HuePicker color={controlSettings.oscillators[1].color} onChange={(color) => { setColor(1, color.hex); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Freq</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[1].frequency]} max={10.0} min={0.0} step={0.01} onValueChange={(value) => { setFrequency(1, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Speed</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[1].speed]} max={30.0} min={0.0} step={0.01} onValueChange={(value) => { setSpeed(1, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Sharp</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[1].sharpen]} max={4.0} min={1.0} step={0.01} onValueChange={(value) => { setSharpen(1, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">X</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[1].center[0]]} max={100.0} min={-100.0} step={0.01} onValueChange={(value) => { setCenterX(1, value[0]); }} />
-        </div>
-        <div className="flex items-center row">
-            <div className="flex w-16 mx-2">Y</div>
-            <Slider className="flex my-4" defaultValue={[controlSettings.oscillators[1].center[1]]} max={100.0} min={-100.0} step={0.01} onValueChange={(value) => { setCenterY(1, value[0]); }} />
-        </div>
+        <OscillatorSettings oscId={0} />
+        <OscillatorSettings oscId={1} />
     </div>
     )
 }
